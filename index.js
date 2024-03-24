dde65cedbef26355d69612473d222bffa74ba9f7
Muhammad-Bilal-Hussain <143377784+Muhammad-Bilal-Hussain@users.noreply.github.com>
2024-03-24 17:03:30 +0500

    create a number guessing game

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "Guesse_Number",
+        type: "number",
+        message: "Please Guesse Any Number"
+    }
+]);
+if (answer.Guesse_Number === randomNumber) {
+    console.log("Congratulation! You Guess Right Number");
+}
+else {
+    console.log("Sorry! You Guesse a Wrong Number");
+}
